c241947bdf36d29d681ded21959ad11bb079cc9e
Dylan McDonald <dmcdonald21@cnm.edu>
2016-07-04 21:01:22 -0600

    Added Webpack of Bootstrap

---
+++ webpack/webpack.common.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+	entry: {
+		'vendor': helpers.root('webpack') + '/vendor.js',
+		'app': helpers.root('webpack') + '/app.js'
+	},
+
+	resolve: {
+		extensions: ['', '.js']
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.html$/,
+				loader: 'html'
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+				loader: 'file?name=assets/[name].[hash].[ext]'
+			},
+			{
+				test: /\.css$/,
+				exclude: helpers.root('src', 'app'),
+				loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+			},
+			{
+				test: /\.css$/,
+				include: helpers.root('src', 'app'),
+				loader: 'raw'
+			}
+		]
+	},
+
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: ['app', 'vendor']
+		}),
+
+		new HtmlWebpackPlugin({
+			template: helpers.root('public_html') + '/index.html'
+		})
+	]
+};
