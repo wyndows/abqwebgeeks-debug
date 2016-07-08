b407b4d1b3451947c50830392623d0f65404b628
Dylan McDonald <dmcdonald21@cnm.edu>
2016-07-07 20:20:34 -0600

    Made the fine print, well fine print ;)

--- public_html/partials/footer.php
+++ public_html/partials/footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6">
-				<p class="text-muted">&copy; 2016 <a href="https://www.deepdivedylan.com/">Dylan McDonald</a>. All source code released under the terms of the <a href="https://www.apache.org/licenses/LICENSE-2.0.txt">Apache 2.0 License</a>. All content released under the terms of the <a href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p>
+				<p class="text-muted"><small>&copy; 2016 <a href="https://www.deepdivedylan.com/">Dylan McDonald</a>. All source code released under the terms of the <a href="https://www.apache.org/licenses/LICENSE-2.0.txt">Apache 2.0 License</a>. All content released under the terms of the <a href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</small></p>
 			</div>
 			<div class="col-md-6">
 				<img class="img-responsive" src="/images/robert-half.png" alt="" />
