4d4475090bf05cf0b05240fe3922d8c0d68912e8
Dylan McDonald <dmcdonald21@cnm.edu>
2016-07-07 21:18:39 -0600

    Structured splash page to use site template

--- public_html/index.php
+++ public_html/index.php
@@ -2,9 +2,11 @@
 $PAGE_TITLE = "Debugging de Deep Dive Dylan";
 require_once("head-utils.php");
 ?>
-	<body>
-		<main class="container">
+	<body class="sfooter">
+		<?php require_once("partials/nav.php"); ?>
+		<main class="container sfooter-content">
 			<h1><i class="fa fa-terminal" aria-hidden="true"></i> Hi ABQ Web Geeks</h1>
 		</main>
+		<?php require_once("partials/footer.php"); ?>
 	</body>
 </html>
