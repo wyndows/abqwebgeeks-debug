2e3459e7301e7ef4215917fb35fac6f26a163b6e
Marlan Ball <wyndows2k@earthlink.net>
2016-07-18 19:21:53 -0600

    First Commit

---
+++ practice.php
@@ -0,0 +1,31 @@
+<?php
+// as an object set
+
+$s = new SplObjectStorage();
+$o1 = new StdClass;
+$o2 = new StdClass;
+$o3 = new StdClass;
+
+$s->attach($o1);
+$s->attach($o2);
+
+var_dump($s->contains($o1));
+var_dump($s->contains($o2));
+var_dump($s->contains($o3));
+
+$s->detach($o2);
+
+var_dump($s->contains($o1));
+var_dump($s->contains($o2));
+var_dump($s->contains($o3));
+
+?>
+
+
+<?php
+// as a map from objects to data
+
+$s = new SplObjectStorage();
+
+$o1 = new StdClass;
+$o2 = new StdClass;
